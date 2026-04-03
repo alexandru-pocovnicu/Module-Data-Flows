05b8512808ba4b17add642937942fc8e24d22481
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-04-03 19:46:52 +0100

    updted syntax to destructure the object

--- Sprint-1/destructuring/exercise-1/exercise.js
+++ Sprint-1/destructuring/exercise-1/exercise.js
@@ -3,6 +3,8 @@ const personOne = {
   age: 34,
   favouriteFood: "Spinach",
 };
+let {name,age,favouriteFood}=personOne;
+
 
 // Update the parameter to this function to make it work.
 // Don't change anything else.
