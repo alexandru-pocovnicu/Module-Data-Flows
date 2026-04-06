2ce8154a63472d61a8de72f14c2d147b3ec9d553
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-04-06 20:52:14 +0100

    Fix: Corrected logic for book read status in submit function

--- debugging/book-library/script.js
+++ debugging/book-library/script.js
@@ -37,7 +37,7 @@ function submit() {
     alert("Please fill all fields!");
     return false;
   } else {
-    let book = new Book(title.value, title.value, pages.value, check.checked);
+    let book = new Book(title.value, title.value, pages.value, check.unchecked);
     myLibrary.push(book);
     render();
   }
