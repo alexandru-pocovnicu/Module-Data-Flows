f24b3b1d56fb5688abf44ef93293e58f49eb5a05
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-04-17 15:17:57 +0100

    Fix: Refactor submit function to use trimmed values for better input validation

--- debugging/book-library/script.js
+++ debugging/book-library/script.js
@@ -27,17 +27,21 @@ const check = document.getElementById("check");
 //check the right input from forms and if its ok -> add the new book (object in array)
 //via Book function and start render function
 function submit() {
+  const titleValue = title.value.trim();
+  const authorValue = author.value.trim();
+  const pagesValue = pages.value.trim();
+
   if (
-    title.value == "" ||
-    author.value == "" ||
-    pages.value == "" ||
-    pages.value < 0 ||
-    !Number.isInteger(+pages.value)
+    titleValue == "" ||
+    authorValue == "" ||
+    pagesValue == "" ||
+    +pagesValue < 0 ||
+    !Number.isInteger(+pagesValue)
   ) {
     alert("Please fill all fields!");
     return false;
   } else {
-    let book = new Book(title.value, author.value, pages.value, check.checked);
+    let book = new Book(titleValue, authorValue, pagesValue, check.checked);
     myLibrary.push(book);
     render();
   }
