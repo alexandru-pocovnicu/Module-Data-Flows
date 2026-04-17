e9452d64e04f69bb2d3a02833e14da8ccd6dc1d1
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-04-17 10:31:45 +0100

    Fix: Remove unnecessary IDs from buttons in render function

--- debugging/book-library/script.js
+++ debugging/book-library/script.js
@@ -75,7 +75,6 @@ function render() {
 
     //add and wait for action for read/unread button
     let changeButton = document.createElement("button");
-    changeButton.id = i;
     changeButton.className = "btn btn-success";
     wasReadCell.appendChild(changeButton);
     let readStatus = "";
@@ -93,7 +92,6 @@ function render() {
 
     //add delete button to every row and render again
     let deleteButton = document.createElement("button");
-    deleteButton.id = i + 5;
     deleteCell.appendChild(deleteButton);
     deleteButton.className = "btn btn-warning";
     deleteButton.innerHTML = "Delete";
